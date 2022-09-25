9595cb656e6ac9dcefc336f4c66adb4dbdae04cf
HulkFront <glauber.araujo@edu.pucrs.br>
2022-09-25 19:36:38 -0300

    fix(RENT-02): delete files

--- dist/CreateCourseService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CreateCurseService {
-    execute({ name, duration = 8, educator }) {
-        console.log(name, duration, educator);
-    }
-}
-exports.default = new CreateCurseService();
